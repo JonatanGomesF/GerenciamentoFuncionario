110e1846cc338d412149ff38e78524403a11d827
jonatan gomes floriano <160983484+jonatancolt@users.noreply.github.com>
2025-05-02 12:24:43 -0300

    criado funcionario/gerente/primeiroFluxograma

---
+++ gerenciamentoFunc/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gerenciamentoFunc
+{
+    public abstract class Funcionario
+    {
+        public string Nome {  get; set; }
+        public int Idade { get; set; }
+        public string Cargo { get; set; }
+        public decimal Salario { get; set; }
+        public string FormaPagamento { get; set; } //(Pix, Débito em Conta, Dinheiro)
+        public string MetodoEntregaPagamento { get; set; } // (Automático, Manual)
+
+        
+        public Funcionario(string nome, int idade, string cargo, decimal salario,
+            string formaPagamento, string metEntrPag)
+        {
+            Nome = nome;
+            Idade = idade;
+            Cargo = cargo;
+            Salario = salario;
+            FormaPagamento = formaPagamento;
+            MetodoEntregaPagamento = metEntrPag;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Nome: {Nome}, Cargo: {Cargo}, Salario: {Salario}," +
+                $"Forma de pagamento Pix, debito em conta ou Dinheiro?{FormaPagamento} " +
+                $"metodo de entrega/pagamento: {MetodoEntregaPagamento}");
+        }
+    }
+}
---
+++ gerenciamentoFunc/Gerente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gerenciamentoFunc
+{
+    public class Gerente : Funcionario
+    {
+        public int Bonus {  get; set; }
+
+        public Gerente(string nome, int idade, string cargo, decimal salario,
+            string formaPagamento, string metEntrPag, int bonus) 
+            : base(nome, idade, cargo, salario, formaPagamento, metEntrPag)
+        {
+            Bonus = bonus;
+        }
+    
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Bonus: {Bonus}");
+        }
+ 
+      
+
+
+
+
+
+
+    }
+}
