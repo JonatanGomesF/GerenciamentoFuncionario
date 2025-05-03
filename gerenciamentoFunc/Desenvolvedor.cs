8ad7d58cedd4cc7e4b7537d9954efe320c01c9c0
jonatan gomes floriano <160983484+jonatancolt@users.noreply.github.com>
2025-05-03 10:49:59 -0300

    Adc 10hrs (extra) de Desenvolvedor

--- gerenciamentoFunc/Desenvolvedor.cs
+++ gerenciamentoFunc/Desenvolvedor.cs
@@ -9,8 +9,8 @@ namespace gerenciamentoFunc
 
         public Desenvolvedor(string nome, int idade, string cargo,
             decimal salario, string formaPagamento, string metEntrPag,
-            int horasExtras, decimal valorPorHorasExtras)
-            : base(nome, idade, cargo, salario, formaPagamento, metEntrPag)
+            int horasExtras, decimal valorPorHorasExtras, decimal impostos)
+            : base(nome, idade, cargo, salario, formaPagamento, metEntrPag, impostos)
         {
             HorasExtras = horasExtras;
             ValorPorHorasExtras = valorPorHorasExtras;
@@ -30,5 +30,14 @@ namespace gerenciamentoFunc
         {
             return $"O pagamento será entregue de forma: {MetodoEntregaPagamento}";
         }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Horas Extras: {HorasExtras}, Valor por Hora Extra: {ValorPorHorasExtras:C}");
+            Console.WriteLine($"Impostos: {CalcularImpostos():C}");
+            Console.WriteLine($"Salário final: {CalcularSalario():C}");
+        }
+
     }
 }
--- gerenciamentoFunc/Program.cs
+++ gerenciamentoFunc/Program.cs
@@ -2,18 +2,18 @@
 {
     internal class Program
     {
-        static List<Funcionario> funcionarios = new List<Funcionario>();
+        static List<Funcionario> funcionarios = new List    <Funcionario>();
         static void Main(string[] args)
         {
 
-            Gerente gerente = new Gerente("Jonatan", 29, 
+            Gerente gerente = new ("Jonatan", 29, 
                 "Gerente de projetos na IBM", 10000m, "pix",
                 "automatico", 2000, 0275);
 
-            Desenvolvedor dev = new Desenvolvedor("Jonatan", 29,
-              "desenvolvedor C# na IBM", 8000m, "Dinheiro", "automatico", 50, 0.1m);
+            Desenvolvedor dev = new ("Jonatan", 29,
+              "desenvolvedor C# na IBM", 8000m, "Dinheiro", "automatico", 10, 50, 0.1m);
 
-            Estagiario joao = new Estagiario("joao", 34,
+            Estagiario joao = new ("joao", 34,
                 "Frontend na IBM", 6000m, "pix", "manual", 0m);
 
             funcionarios.Add(gerente);
